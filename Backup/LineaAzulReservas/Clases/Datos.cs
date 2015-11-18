7a5bc81c18f3b197145145ee41d6ec0fe1e899df
Jose Osses <jooswebs@gmail.com>
2015-11-18 17:48:24 -0300

    Base de datos creada
    recordar que en la base de datos membership la columna web_admin es
    FOREIGN KEY de member_rol (rol_id).

---
+++ Backup/LineaAzulReservas/Clases/Datos.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LineaAzulReservas.Clases
+{
+
+    /*aqui esta la clase coneccion..
+     
+     falta esperar lo de base de datos para empezar a trabajar 
+     
+     por si acaso entre hoy ó mañana nos entregan */
+    public class Datos
+    {
+
+        private string cadena = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\diarioOnlineBD.mdf;Integrated Security=True";
+        public SqlConnection cn;
+        public SqlDataAdapter da;
+        public DataSet ds = new DataSet();
+        public SqlCommand comando;
+
+        private void Conectar()
+        {
+
+            cn = new SqlConnection(cadena);
+        }
+
+        public Datos()
+        {
+            Conectar();
+        }
+
+        public DataTable consultaTabla(string tabla)
+        {
+            string sql = "select * from " + tabla;
+            da = new SqlDataAdapter(sql, cn);
+            DataSet dts = new DataSet();
+            da.Fill(dts, tabla);
+            DataTable dt = new DataTable();
+            dt = dts.Tables[tabla];
+
+            return dt;
+
+
+        }
+
+        public bool insertar(string sql)
+        {
+            cn.Open();
+            comando = new SqlCommand(sql, cn);
+            int i = comando.ExecuteNonQuery();
+            cn.Close();
+
+            if (i > 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+    }
+}
